d111267206b89cd58728422351d9fe023c9eacfe
netomatiazi <netoomn@gmail.com>
2020-09-29 16:12:55 -0300

    Adding nodemon dependency

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+//Instanciando o express que retornará a aplicação
+const app = express()
+
+app.listen(3000, () => {
+    console.log('Beckend executando...')
+})
+
