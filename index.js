e911a2e24eacb976a834ee1c4609dfb0ecd84194
netomatiazi <netoomn@gmail.com>
2020-09-30 10:06:13 -0300

    applying types of responses

--- index.js
+++ index.js
@@ -6,7 +6,36 @@ const app = express()
 // Permite tanto executar um get quanto um post
 // Uma callback devolve a resp
 app.use('/opa',(req, res) => {
-    res.send('Estou <b>bem</b>!.')
+    // Passando array de objetos
+    // res.json([
+    //     {id: 7, name: "Ana", position: 1 },
+    //     {id: 34, name: "Bia", position: 2 },
+    //     {id: 73, name: "Carlos", position: 3 }
+    // ])
+
+    //Passando um objeto com informações da requisição
+    res.json({
+        data: [
+            {id: 7, name: "Ana", position: 1 },
+            {id: 34, name: "Bia", position: 2 },
+            {id: 73, name: "Carlos", position: 3 }
+        ],
+        count: 30, // POderia retornar um count para dizer qts registros tem na base
+        skip:0, // tbm um skip para trabalhar com paginação
+        limit: 3, // Limitando numero de páginas
+        status: 200 // Pode conter um status
+
+    })
+
+    /*
+    Enviando resposta em json
+    res.json({
+        name:"Ipad 32GB",
+        price:2135.90,
+        discount: .12    
+    })
+    */
+    // res.send('Estou <b>bem</b>!.')
 })
 
 //Backend executando na porta 3000
