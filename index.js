f8dd7e66b7020ab6736f1b17a1d0128858020328
netomatiazi <netoomn@gmail.com>
2020-09-30 09:36:17 -0300

    Handling the first request

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 //Instanciando o express que retornará a aplicação
 const app = express()
 
+// Método use recebe como parâmetro a requisição e resposta
+// Permite tanto executar um get quanto um post
+// Uma callback devolve a resp
+app.use('/opa',(req, res) => {
+    res.send('Estou <b>bem</b>!.')
+})
+
+//Backend executando na porta 3000
 app.listen(3000, () => {
     console.log('Beckend executando...')
 })
